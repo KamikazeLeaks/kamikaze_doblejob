179cb9893fe8037c97e7a8a5d126015f14f61486
lizarragaeneko <67381650+lizarragaeneko@users.noreply.github.com>
2021-04-22 21:10:55 +0200

    Update __resource.lua

--- __resource.lua
+++ __resource.lua
@@ -2,13 +2,17 @@ fx_version 'adamant'
 
 game 'gta5'
 
-description 'Free Chema Script Doble JOB'
+description 'Free KamikazeLeaks 2 Jobs Script'
 
-client_script 'client/main.lua'
+client_script {
+    'client/main.lua',
+    'config.lua'
+    }
 
 version '1.0.1'
 
 server_scripts {
     'server/main.lua',
-    '@mysql-async/lib/MySQL.lua'
+    '@mysql-async/lib/MySQL.lua',
+    'config.lua'
 }
